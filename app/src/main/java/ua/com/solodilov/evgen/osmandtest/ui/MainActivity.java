60ffcba4d25bd1139ac0216ab75673248a3f9338
Solod <jack_killer@mail.ru>
2017-05-10 20:44:15 +0300

    arrow back

--- app/src/main/java/ua/com/solodilov/evgen/osmandtest/ui/MainActivity.java
+++ app/src/main/java/ua/com/solodilov/evgen/osmandtest/ui/MainActivity.java
@@ -4,21 +4,40 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import ua.com.solodilov.evgen.osmandtest.R;
 import ua.com.solodilov.evgen.osmandtest.interfaces.OnRegionFragment;
 
-public class MainActivity extends AppCompatActivity implements OnRegionFragment {
+public class MainActivity extends AppCompatActivity implements OnRegionFragment, FragmentManager.OnBackStackChangedListener {
     private FragmentManager mFragmentManager = getSupportFragmentManager();
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mFragmentManager.addOnBackStackChangedListener(this);
         if (savedInstanceState == null) {
             addFirstFragment();
+        }
+    }
+
+    @Override
+    public void onBackStackChanged() {
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+        if (count > 0) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true); // show back button
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBackPressed();
+                }
+            });
+        } else {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);//show hamburger
         }
     }
 
